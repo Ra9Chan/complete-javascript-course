cf0a5ec365eb13a8f4c5cbf017bbfe190ebb08f9
Ra9Chan <rdwrayc@outlook.com>
2022-08-13 11:05:07 +0800

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,70 @@
+// const markHeight = 98;
+// const markTall = 1.98
+// const markBMI = markHeight / markTall ** 2;
+
+// const wrayHeight = 68;
+// const wrayTall = 1.78;
+// const wrayBMI = wrayHeight / wrayTall ** 2;
+
+
+// if(wrayBMI>markBMI){
+//     console.log(`Mark's BMI is ${markBMI},bigger than Wray`);
+// }else{
+//     console.log(`Wray's BMI is ${wrayBMI},bigger than Mark`);
+
+// }
+
+// const visionSarah = true
+// const licenseSarah = true
+
+// if(visionSarah && licenseSarah) console.log("Yep");
+
+// const age = 25;
+// if(age >= 20 && age <30){
+//     console.log("Yepp");
+// }
+
+// const hasDriversLicense = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// const isTired = false;
+// console.log(hasDriversLicense || hasGoodVision || !isTired);
+
+// const shouldDrive = (hasDriversLicense && hasGoodVision && !isTired);
+
+// if(shouldDrive){
+//     console.log("Sarah is able to driver!");
+// }else {
+//     console.log(`Someone else should driver`);
+// }
+
+
+// challenge #3
+
+ const averageDolphons =(96+108+89)/3;
+ const averageKoalas = (97+112+101)/3;
+
+ const result = 0;
+ if(averageDolphons>averageKoalas){
+    console.log("winner is Dolphons🐬!");
+ }else if(averageDolphons < averageKoalas){
+    console.log("winner is Koalas🐨!");
+ }else if(averageDolphons === averageKoalas){
+    console.log(`Both win the trophy🏆!`);
+ }
+ const averageDolphonsCopy =(97+112+101)/3;
+ const averageKoalasCopy = (109+95+123)/3;
+
+ if(averageDolphonsCopy>averageKoalasCopy && averageDolphonsCopy>=100){
+    console.log("winner is Dolphons🐬!");
+ }else if(averageDolphonsCopy<averageKoalasCopy && averageKoalasCopy>=100){
+    console.log("winner is Koalas🐨!");
+ }else{
+    console.log(`Both win the trophy🏆!`);
+
+ }
+ 
