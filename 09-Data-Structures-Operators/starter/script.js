46d029b5da59ab5531f210fbdf273a41fa6da351
Ra9Chan <rdwrayc@outlook.com>
2022-09-07 15:41:27 +0800

    09

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,7 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +29,53 @@ const restaurant = {
     },
   },
 };
+
+const arr = [1, 2, ...[3, 4]];
+const { sat, ...othersObj } = restaurant.openingHours;
+console.log(sat, othersObj);
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+const x = [23, 5, 7];
+add(...x);
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+// const arr = [2, 3, 4];
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// //Nested destructuring
+// const nested = [2, 4, [5, 6]];
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// //Default values
+// const [p = 1, q = 1, r = 1] = [5, 6];
+// console.log(p, q, r);
