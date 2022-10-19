03f45b4aa9f023eff16775efcff530cc86bc6d7e
Ra9Chan <rdwrayc@outlook.com>
2022-10-19 10:21:35 +0800

    Changes

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -43,19 +43,125 @@
 // newPssprot(wray);
 // checkIn(flight, wray);
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// // higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+
+// // Js callback all the time
+// const high5 = function () {
+//   console.log('😛');
+// };
+// document.body.addEventListener('click', high5);
+
+// ['wray', 'john', 'k'].forEach(high5);
+
+//challenge
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+// const greeterHey = greet('Hey');
+// greeterHey('Wray');
+// greeterHey('John');
+// greet('Hello')('Ra9c');
+
+const lufthansa = {
+  airline: 'Lifthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function (){}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+lufthansa.book(239, 'Ra9c');
+lufthansa.book(635, 'John');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
 };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
+const book = lufthansa.book;
 
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string ${fn(str)}`);
+// Dose Not work
+// book(23, 'Sarah');
 
-  console.log(`Transformed by: ${fn.name}`);
+// call method
+book.call(eurowings, 23, 'Sarah');
+console.log(eurowings);
+book.call(lufthansa, 239, 'Cherry');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
 };
-transformer('JavaScript is the best!', upperFirstWord);
+book.call(swiss, 583, 'Mary');
+console.log(swiss);
+
+//apply method
+const flightData = [583, 'Cooper'];
+// (not used)
+// book.apply(swiss, flightData);
+book.call(swiss, ...flightData);
+console.log(swiss);
+
+//bind method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(23, 'King');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jc');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+lufthansa.buyPlane();
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+//bind(null, ...arg)
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(200));
+
+//challenge
+const addTaxRate = rate => value => value + value * rate;
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(200));
